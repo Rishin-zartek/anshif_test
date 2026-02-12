87ad90c0419749cb535c97a1d3c0c9fb9a177e3f
minihands <minihands@minitap.ai>
2026-02-12 04:18:45 +0000

    feat(data): add Kerala foods JSON database and data seeder service

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,13 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/theme/app_theme.dart';
 import 'core/router/app_router.dart';
 import 'core/database/hive_config.dart';
+import 'core/services/data_seeder.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
   await HiveConfig.initialize();
+  await DataSeeder.seedData();
   
   runApp(const ProviderScope(child: MyApp()));
 }
